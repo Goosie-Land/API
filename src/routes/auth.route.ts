65b1fc39139ba38c8b970c2c84b73e2ad1a8513e
Elie Lajoinie <elielajoinie24@gmail.com>
2025-03-27 13:51:49 +0100

    fix: get me

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -8,6 +8,6 @@ const userController = new UserController();
 router.post('/login', (req, res, next) => userController.logUser(req, res, next));
 router.post('/register', (req, res, next) => userController.createUser(req, res, next));
 router.post('/refresh', (req, res, next) => userController.refreshToken(req as EncodedRequest, res, next));
-router.get('/me', (req, res, next) => userController.getMe(req as EncodedRequest, res, next))
+router.post('/me', (req, res, next) => userController.getMe(req as EncodedRequest, res, next))
 
 export default router;
